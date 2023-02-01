019969de1bdda4715b8607c69e756e4219718e24
markus b <mnogo_bukof@mail.ru>
2023-02-01 15:11:02 +0700

    справочник5

---
+++ log.py
@@ -0,0 +1,6 @@
+import datetime 
+
+def log_cash(rezhim, result):
+    with open ('log.txt', 'a', encoding='utf-8') as file:
+        file.write(f'{rezhim} = {result}. Время запроса: {str(datetime.datetime.now())} \n')
+        
